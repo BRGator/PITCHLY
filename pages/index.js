13f914f0e8fd08a2c2e6cd351c577db43f52401e
BRGator <A@A3.LTD>
2025-08-30 01:36:19 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-// Placeholder for index.js
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welcome to Pitchly</h1>
+      <p>This is the homepage. Content coming soon.</p>
+    </div>
+  );
+}
